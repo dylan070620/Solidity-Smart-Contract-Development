d717df621a6e136f62c856cef6472a15221fecdc
Dylan Zhang <dylan207620@gmail.com>
2026-01-14 16:52:03 -0500

    -

--- test.sol
+++ test.sol
@@ -3,5 +3,12 @@ pragma solidity ^0.8.0;
 
 contract hello_web3{
     string public _string = "hello  web3! hhhhhhh";
+    function fuck( uint times )public pure returns(uint){
+        return times;
+    }
+    function getbalance() public view returns(uint){
+        return address(this).balance;
+    }
 }
 //nb
+
